55c388cbed0dc93f77ca1e605b090394b89f5dfa
Javier <javirimo@hotmail.com>
2021-03-09 23:49:32 -0600

    JAVIOR

--- PruebaGIT/Punto.h
+++ PruebaGIT/Punto.h
@@ -9,6 +9,12 @@ public:
 	Punto(int x, int y) : x(x), y(y) {}
 	~Punto() = default;
 
+    int getX() const { return x; }
+    void setX(int x) { this->x = x; }
+
+    int getY() const { return y; }
+    void setY(int y) { this->y = y; }
+
 
 
 };
